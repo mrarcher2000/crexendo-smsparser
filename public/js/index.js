031665b080edb31abf011802fdbdac04aaabb44a
Archer Nicholson <archernich@gmail.com>
2023-03-14 06:56:15 -0700

    not working buttons to sms convo

--- public/js/index.js
+++ public/js/index.js
@@ -8,15 +8,6 @@ var formData = document.querySelector('#userData');
 
 let url = 'https://crexendo-core-021-las.cls.iaas.run/ns-api/?object=conversation&action=read';
 
-// let options = {
-//   method: 'POST',
-//   headers: {
-//     'Content-Type': 'application/json',
-//     Authorization: 'Bearer 25d810d1ade08e833acde4214d2e4b70'
-//   },
-//   body: '{"domain":"Archer_Nicholson_Lab","user":"100"}'
-// };
-
 var reqbody = '{"domain":"Archer_Nicholson_Lab","user":"100"}';
 
 function checkData() {
@@ -27,6 +18,11 @@ function checkData() {
 
 var xhttp = new XMLHttpRequest();
 let xmlData;
+
+
+var msghttp = new XMLHttpRequest();
+
+var conversationList = [];
 
 function parseViaSession(xmlData) {
   for (let i=0; i < xmlData.length; i++) {
@@ -39,12 +35,41 @@ function parseViaSession(xmlData) {
     // let last_mesg = parent.getElementsByTagName("last_mesg").textContent;
     appendDataElement.insertAdjacentHTML("afterend", 
       `<div>
+      <button id="searchresult[${i}]" type="button" class="btn btn-secondary btn-lg btn-block">${remote} : Last Message: ${last_mesg} at ${last_timestamp}</button>
       <h4>${remote} : <span>Last Message: ${last_mesg} at ${last_timestamp}</span></h4>
       <br />`
     );
+    conversationList.push(`${parent.childNodes[0].textContent}`);
     // console.log(parent.childNodes[5].textContent);
   }
+
+  console.log(conversationList);
+  createConversationLinks(conversationList);
 }
+
+var createConversationLinks = function(conversationList) {
+  for (i=0;i<conversationList.length;i++) {
+    document.querySelector(`#searchresult[${i}]`).addEventListener("click", (e) => {
+      e.preventDefault();
+      goToConversation(conversationList[i]);
+    });
+
+  }
+}
+
+msghttp.onreadystatechange = function() {
+  if (this.readyState == 4 && this.status == 200) {
+    console.log(msghttp.responseXML);
+  }
+}
+
+var goToConversation = function(session_id) {
+  let msgReqBody = `{"domain": "${domainInput}", "user":"${userInput}", "session_id": "${session_id}", "limit":"1000"}`;
+  msghttp.open("POST", "https://crexendo-core-021-las.cls.iaas.run/ns-api/?object=message&action=read");
+  msghttp.setRequestHeader("Authorization", "Bearer d2375cb0aed7615cee01c29d9833651f");
+  msghttp.send(msgReqBody);
+}
+
 
 xhttp.onreadystatechange = function() {
   if (this.readyState == 4 && this.status == 200) {
@@ -62,7 +87,7 @@ xhttp.onreadystatechange = function() {
 
 function fetchData() {
   xhttp.open("POST", url);
-  xhttp.setRequestHeader("Authorization", "Bearer ccc2212e03770a0a7d2f38d84533ad91");
+  xhttp.setRequestHeader("Authorization", "Bearer d2375cb0aed7615cee01c29d9833651f");
   xhttp.send(reqbody);
 }
 
