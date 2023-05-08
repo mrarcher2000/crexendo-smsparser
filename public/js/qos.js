5b0e55435e7fdb02b1834b38543ca8c411c57fe0
Archer Nicholson <archernich@gmail.com>
2023-05-08 12:07:21 -0700

    added login modal to qos.html and qos.js

--- public/js/qos.js
+++ public/js/qos.js
@@ -8,23 +8,56 @@ const domain = document.querySelector('#domain');
 const cdrsubmit = document.querySelector('#cdrsubmit');
 const errorBlockElement = document.querySelector('#error-block');
 const successBlockElement = document.querySelector('#success-block');
+var loginFormUser = document.querySelector('#loginFormUser');
+var loginFormPassword = document.querySelector('#loginFormPassword');
+var loginFormSubmit = document.querySelector('#loginFormSubmit');
+var loginFormModal = new bootstrap.Modal(document.querySelector('#modalLoginForm'));
 
 var ns_access = "";
 
 window.onload = function() {
-    fetch('https://crexendo-core-021-las.cls.iaas.run/ns-api/oauth2/token/?grant_type=password&client_id=archertest&client_secret=90056b1f11f8c87fff30fd1b5acafd04&username=anicholson@crexendo.com&password=Crexendo2022!', {
-        method: "POST"
-    })
-    .then((response) => response.json())
-    .then((response) => {
-        if (response.access_token) {
-            ns_access = response.access_token;
-            console.log('Server Connection Successful');
-        }
-    })
-    .catch((err) => {
-        console.log(`Error received from Server: ${err}`);
-    })
+    // fetch('https://crexendo-core-021-las.cls.iaas.run/ns-api/oauth2/token/?grant_type=password&client_id=archertest&client_secret=90056b1f11f8c87fff30fd1b5acafd04&username=anicholson@crexendo.com&password=Crexendo2022!', {
+    //     method: "POST"
+    // })
+    // .then((response) => response.json())
+    // .then((response) => {
+    //     if (response.access_token) {
+    //         ns_access = response.access_token;
+    //         console.log('Server Connection Successful');
+    //     }
+    // })
+    // .catch((err) => {
+    //     console.log(`Error received from Server: ${err}`);
+    // })
+
+    loginFormModal.show();
+  loginFormSubmit.focus();
+  loginFormPassword.focus();
+
+  loginFormSubmit.addEventListener("click", (e) => {
+    e.preventDefault();
+    
+  const response = fetch(`https://crexendo-core-021-las.cls.iaas.run/ns-api/oauth2/token/?grant_type=password&client_id=archertest&client_secret=90056b1f11f8c87fff30fd1b5acafd04&username=${loginFormUser.value}&password=${loginFormPassword.value}`, {
+    method: "POST",
+  })
+  .then((response) => response.json())
+  .then((response) => {
+    if (response.access_token) {
+      ns_access = response.access_token;
+      console.log('Access Token Received from Netsapiens Server');
+      loginFormModal.hide();
+    } else {
+      let incorrectLoginFunc = function() {
+        alert('Incorrect Username or Password! Please try again');
+        window.location.reload();
+      };
+      setTimeout(incorrectLoginFunc(), 5000);
+    }
+  })
+  .catch(function() {
+    alert('Incorrect Username or Password!');
+  });
+  });
 };
 
 // New XML HTTP Request to pull CDR Information from Server
